268d3fbb6cff51704be9949b14ae3147ae26108d
issatarane1-art <issa.tarane1@gmail.com>
2026-09-22 21:11:49 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import librosa
 import soundfile as sf
-from PIL import Image
 from gtts import gTTS
 from flask import Flask, render_template, request, send_from_directory
 
@@ -23,11 +22,17 @@ def process():
     
     if text_input:
         try:
-            # استفاده از زبان انگلیسی به عنوان جایگزین پایدار روی سرورهای ابری خارجی برای جلوگیری از خطای پشتیبانی زبان
+            # ذخیره با کتابخانه gTTS و تبدیل به فرمت WAV سازگار با تمام مرورگرها
             tts = gTTS(text=text_input, lang='en', slow=False)
-            audio_name = "speech_output.mp3"
+            mp3_path = os.path.join(OUTPUT_FOLDER, "temp.mp3")
+            audio_name = "speech_output.wav"
             audio_path = os.path.join(OUTPUT_FOLDER, audio_name)
-            tts.save(audio_path)
+            
+            tts.save(mp3_path)
+            
+            # تبدیل فایل به WAV برای اینکه مدت زمان در پلیر مرورگر دقیق نشان داده شود
+            y, sr = librosa.load(mp3_path, sr=None)
+            sf.write(audio_path, y, sr)
             
             return render_template('index.html', 
                                    speech_success="متن شما با موفقیت به فایل صوتی تبدیل شد!", 
