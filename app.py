e7befb1b6e13ae735de2ff77e97a980dac866557
issatarane1-art <issa.tarane1@gmail.com>
2026-09-23 12:51:36 +0330

    Update app.py

--- app.py
+++ app.py
@@ -14,51 +14,50 @@ os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
 def process():
-    if 'file' not in request.files:
-        return render_template('index.html', error="لطفاً یک فایل صوتی انتخاب کنید.")
-        
-    file = request.files['file']
-    if file.filename == '':
-        return render_template('index.html', error="فایلی انتخاب نشده است.")
-        
-    try:
-        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
-        file.save(file_path)
-        
-        # پردازش سریع و سبک برای جلوگیری از فریز شدن سرور
-        y, sr = librosa.load(file_path, sr=22050, duration=30.0, mono=False)
-        
-        if y.ndim > 1:
-            vocals = (y[0] + y[1]) / 2
-            instrumental = y - vocals
-        else:
-            vocals = y
-            instrumental = y * 0.5
-        
-        unique_id = int(time.time())
-        inst_name = f"instrumental_{unique_id}.wav"
-        vocal_name = f"vocals_{unique_id}.wav"
-        
-        inst_path = os.path.join(OUTPUT_FOLDER, inst_name)
-        vocal_path = os.path.join(OUTPUT_FOLDER, vocal_name)
-        
-        sf.write(inst_path, instrumental.T, sr)
-        sf.write(vocal_path, vocals.T, sr)
-        
-        return render_template('index.html', 
-                               success="جداسازی صدا و موزیک با موفقیت انجام شد!", 
-                               instrumental=inst_name, 
-                               vocals=vocal_name)
-    except Exception as e:
-        return render_template('index.html', error="خطا در پردازش فایل. لطفاً فایل صوتی کوچکتری انتخاب کنید.")
+    if 'file' not in request.files:
+        return render_template('index.html', error="لطفاً یک فایل صوتی انتخاب کنید.")
+        
+    file = request.files['file']
+    if file.filename == '':
+        return render_template('index.html', error="فایلی انتخاب نشده است.")
+        
+    try:
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        file.save(file_path)
+        
+        y, sr = librosa.load(file_path, sr=22050, duration=30.0, mono=False)
+        
+        if y.ndim > 1:
+            vocals = (y[0] + y[1]) / 2
+            instrumental = y - vocals
+        else:
+            vocals = y
+            instrumental = y * 0.5
+        
+        unique_id = int(time.time())
+        inst_name = f"instrumental_{unique_id}.wav"
+        vocal_name = f"vocals_{unique_id}.wav"
+        
+        inst_path = os.path.join(OUTPUT_FOLDER, inst_name)
+        vocal_path = os.path.join(OUTPUT_FOLDER, vocal_name)
+        
+        sf.write(inst_path, instrumental.T, sr)
+        sf.write(vocal_path, vocals.T, sr)
+        
+        return render_template('index.html', 
+                               success="جداسازی صدا و موزیک با موفقیت انجام شد!", 
+                               instrumental=inst_name, 
+                               vocals=vocal_name)
+    except Exception as e:
+        return render_template('index.html', error="خطا در پردازش فایل. لطفاً فایل صوتی کوچکتری انتخاب کنید.")
 
 @app.route('/download/<filename>')
 def download_file(filename):
-    return send_from_directory(OUTPUT_FOLDER, filename, as_attachment=True)
+    return send_from_directory(OUTPUT_FOLDER, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)
